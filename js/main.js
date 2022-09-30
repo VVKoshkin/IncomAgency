2c36927deff2e2d42985b781fb163616d61e5919
V.Koshkin <humble94@mail.ru>
2022-09-30 17:20:36 +0300

    Finished layout and adaptive

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(() => {
     $('.navbar-hamburger').click(() => {
         $('.navbar-elements').toggle();
     });
+
+    if($(window).width() <= 770) {
+        $('.advantage-description.ml50').removeClass('ml50')
+    }
 });
